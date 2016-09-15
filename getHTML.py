7317b3d1f5a879a055d77930cc4e0f615c2ce83d
neigrok <samgold321@gmail.com>
2016-09-15 22:41:26 +0400

    Update getHTML.py
    another request

--- getHTML.py
+++ getHTML.py
@@ -2,14 +2,16 @@ import requests
 
 def getHTML():
     params = {
+        "n": "2",
         "st": "2",
-        "r": "6",
-        "d": "3179",
-        "D": "3180",
-        "t": "текст"
+        "r": "5",
+        "d": "3135",
+        "D": "3137",
+        "q": "текст",
     }
     r = requests.get("http://pikabu.ru/search.php", params=params)
-
+    print(r)
+    
     return r.text
-
-#test
+    
+#тест
