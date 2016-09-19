77162f3038e906cd9109c45017c7942d36a539d0
neigrok <samgold321@gmail.com>
2016-09-19 18:58:47 +0300

    folder creating added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import storyClass
 import parsev2
+import os
 
 storyClass.Story.initstories(parsev2.collectworks())
-
+if not os.path.exists('Stories/'):
+    os.makedirs('Stories/')
 for story in storyClass.Story.stories:
         f = open('Stories/' + story.getheader() + ".txt", mode="w", encoding="UTF-8")
         f.write(story.getbody())
